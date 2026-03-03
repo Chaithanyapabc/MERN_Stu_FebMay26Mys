758f58d306ae2288a151e7884b3901840378bb08
Chaithanyapabc <pchaithanya684@gmail.com>
2026-03-03 15:43:21 +0530

    Arthmatic Operators

---
+++ W3/D2/P1.js
@@ -0,0 +1,14 @@
+//Arthmatic Operators
+const a = 2, b = 3;
+let c = 0;
+console.log("Arthmatic Operators");
+console.log("a+b: ",c=a+b);
+console.log("a-b: ",a-b);
+console.log("a*b: ",a*b);
+console.log("a/b: ",a/b);
+console.log("a%b: ",a%b);//modules
+console.log("a**b: ",a**b);//Exponential
+console.log("a++: ",a++);//post-incriment
+console.log("++a: ",++a);//pre-increment
+console.log("a--: ",a--);//post-decrement
+console.log("--a: ",--a);//pre-decrement
