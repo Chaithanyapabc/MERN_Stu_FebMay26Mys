1c66c3246c129b8e8255bc7889a6828a37f61104
Chaithanyapabc <pchaithanya684@gmail.com>
2026-03-03 15:44:10 +0530

    Arthmatic Operators

---
+++ W3/D2/P2.js
@@ -0,0 +1,12 @@
+// Comparison Operators
+
+console.log("Comparison Operators");
+let x = 10, y = 3;
+console.log("x == y: ", x == y);//Equal to
+console.log("x === y: ", x === y);//Strict Equal to
+console.log("x != y: ", x != y);//Not Equal to
+console.log("x !== y: ", x !== y);//Strict Not Equal to
+console.log("x > y: ", x > y);//Greater than
+console.log("x < y: ", x < y);//Less than
+console.log("x >= y: ", x >= y);//Greater than or Equal to
+console.log("x <= y: ", x <= y);//Less than or Equal to
