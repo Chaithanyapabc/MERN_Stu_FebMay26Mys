2112ebe06dd9c6432e1b22d9d25a435078426654
Chaithanyapabc <pchaithanya684@gmail.com>
2026-03-03 15:46:55 +0530

    conditional statements,switch statement,Type conversion

---
+++ W3/D2/P4.js
@@ -0,0 +1,39 @@
+/*//conditional statements
+let age = 20;
+if(age < 13){
+    console.log("Child");
+}
+else if(age < 18){
+    console.log("Teenager");
+}
+else{
+    console.log("Adult");
+}
+*/
+
+
+/*//switch statement
+console.log("Switch Statements");
+const day = "Tuesday";
+switch(day){
+    case "Monday":
+        console.log("Start of the week");
+        break;
+    case "Wednesday":
+        console.log("mid of the week");
+        break;
+        case "Friday":
+        console.log("End of the week");
+        break;
+    default:
+        console.log("Some day in the week");
+        break;
+}
+*/
+
+
+
+//Type conversion
+console.log("Type Conversion");
+let n = Number("ABC");
+console.log("n: ", n, "Type of n: ", typeof n, "isNAN: ", isNaN(n));
