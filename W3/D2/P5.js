6b94eb6732f2254c2dc5ed05ac8c82a7d10cf61e
Chaithanyapabc <pchaithanya684@gmail.com>
2026-03-04 07:27:01 +0530

    function definition/declaration

---
+++ W3/D2/P5.js
@@ -0,0 +1,9 @@
+// Function Declaration
+//function greet(name);
+greet("Chaithanya");//holistic
+//function definition/declaration
+function greet(name){
+    console.log("Hello, "+name+"!");
+}
+//function
+greet("Chaithanya");
