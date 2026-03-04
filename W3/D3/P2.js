f32ed02024430ff01c87227489436e68984e5417
Chaithanyapabc <pchaithanya684@gmail.com>
2026-03-04 12:41:35 +0530

    While Loop,Do While Loop

---
+++ W3/D3/P2.js
@@ -0,0 +1,14 @@
+//while loop
+console.log("While Loop examples");
+let count = 0;
+while (count < 5) {
+    console.log("count= ", count);
+    count++; //count--; is a infinite Loop
+}
+
+//Do While Loop
+let value = 0;
+do {
+    console.log("Value :",value);
+    value++;
+} while(value<3);
