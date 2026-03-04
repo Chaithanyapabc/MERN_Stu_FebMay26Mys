86392326440d3b0e39e83cf4abb77f98cc473265
Chaithanyapabc <pchaithanya684@gmail.com>
2026-03-04 12:44:34 +0530

    Basics of objects in JavaScript

---
+++ W3/D3/P5.js
@@ -0,0 +1,10 @@
+//Basics of objects in JavaScript
+const person = {
+    name : "P Chaithanya",
+    age : 22,
+    isStudent : true
+};
+//console.log("Person :",person);
+console.log("Name :",person.name);
+console.log("Age :",person["age"]);
+console.log("Is Student :",person["isStudent"]);
