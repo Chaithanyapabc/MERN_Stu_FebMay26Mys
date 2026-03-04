522a4afc5ca962c6019e683e1bab0d8de7959ca3
Chaithanyapabc <pchaithanya684@gmail.com>
2026-03-04 14:37:07 +0530

    Basics of objects in JavaScript

--- W3/D3/P5.js
+++ W3/D3/P5.js
@@ -8,3 +8,19 @@ const person = {
 console.log("Name :",person.name);
 console.log("Age :",person["age"]);
 console.log("Is Student :",person["isStudent"]);
+
+//Add a new property
+person.city = "Mysore";
+console.log("Person :",person);
+
+//Modify an existing property
+person.age = 23;
+//delete a property
+delete person.isStudent;
+console.log("Person :",person);
+//Object Constructor
+const car = new Object();
+car.make = "Audi";
+car.model = "A4";
+car.year = 2026;
+console.log("Car :",car);
