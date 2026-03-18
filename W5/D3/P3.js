a481a45a7788f20ffdfa588e55660ead5ff46f48
Chaithanyapabc <pchaithanya684@gmail.com>
2026-03-18 10:41:41 +0530

    Handling errors with try/catch

---
+++ W5/D3/P3.js
@@ -0,0 +1,23 @@
+//Handling errors with try/catch
+function loadCustomerProfile(){
+    return new Promise(function(resolve,reject){
+        const isServiceAvailable = true;
+
+        if(isServiceAvailable){
+            resolve("Success! customer profile loaded.");
+        }
+        else{
+            reject("Unsuccessful! customer profile loaded.");
+        }
+    });
+}
+async function showCustomerProfile(){
+    try{
+        const message = await loadCustomerProfile();
+        console.log(message);
+    }
+    catch(error){
+        console.log("Error:",error);
+    }
+}
+showCustomerProfile();
