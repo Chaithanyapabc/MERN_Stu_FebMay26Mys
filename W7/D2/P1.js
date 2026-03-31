29ade1ac6a2b232abbae90cb19fedbb2adf41420
Chaithanyapabc <pchaithanya684@gmail.com>
2026-03-31 17:12:01 +0530

    Routing : Nested routes

--- W7/D2/P1.js
+++ W7/D2/P1.js
@@ -24,9 +24,36 @@ apiRouter.get("/orders",function(req,res){
     });
 });
 
-//Mount the router under the/api base path
+//Mount the router under the /api base path
 app.use("/api",apiRouter);
 
+//***New Router***/
+//products router(handles/api/products/...)
+const productsRouter = express.Router();
+
+// /api/products           ->create product
+productsRouter.post("/",(req,res) => {
+    res.json({
+        route:"/api/products",
+        message:"Create products",
+    });
+});
+
+// /api/products/:id           ->delete product
+productsRouter.delete("/:id",(req,res) => {
+    res.json({
+        route:`/api/products/${req,URLSearchParams.id}`,
+
+        message:"Delete product",
+    });
+});
+
+//mount products router under /api/products
+apiRouter.use("/products", productsRouter);
+
+//curl -X POST http://localhost:4000/api/products -H "Content-Type:application/json" -d "{"name":"Pen","price":10}"
+//curl -X DELETE http://localhost:4000/api/products/123
+//http://localhost:4000/api/users
 app.listen(4000,function(){
     console.log("Express server is running at http://localhost:4000");
 });
