67c75da65cd01a460993e372438ffd69ccc09208
Chaithanyapabc <pchaithanya684@gmail.com>
2026-04-03 11:06:57 +0530

    Cookie is used to track session ID

---
+++ W7/D5/P1.js
@@ -0,0 +1,16 @@
+//Cookie is used to track session ID
+
+//Simulated server-side session store
+const sessionStore = {
+    "abc123" :{
+        userId:101,
+        username:"Chaithanya",
+        role:"Student"
+    }
+};
+
+//Simulated browser cookie value
+const browserCookieSessionId = "abc123";
+
+const sessionData = sessionStore[browserCookieSessionId];
+console.log("Server-side session data:",sessionData);
