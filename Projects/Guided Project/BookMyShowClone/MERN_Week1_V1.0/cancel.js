28ea3c765f648f7cc885b5809baccbca81c9d2d2
Chaithanyapabc <pchaithanya684@gmail.com>
2026-04-07 09:00:33 +0530

    To cancel the existing booking if exists

--- Projects/Guided Project/BookMyShowClone/MERN_Week1_V1.0/cancel.js
+++ Projects/Guided Project/BookMyShowClone/MERN_Week1_V1.0/cancel.js
@@ -1,35 +1,39 @@
-//cancel.js 
-//To cancel the existing booking if exists
+//cancel.js
+// To cancel the existing booking if exists
 const bookingEmitter = require("./events");
-const { getCurrentBooking, clearCurrentBooking } = require("./booking");
+const {getCurrentBooking,clearCurrentBooking} = require("./booking");
 
-function cancelBooking(){
-    const currentBooking = getCurrentBooking();
-    if(!booking){
-        bookingEmitter.emit("booking Failed","No active booking found to cancel.");
+function cancelBooking(movies){
+    const booking = getCurrentBooking();
+
+    if (!booking) {
+        bookingEmitter.emit("bookingFailed","No booking found to cancel.");
         return null;
     }
-    const movie = movie.find((m).id === booking.movieId);
-    if(!movie){
-        bookingEmitter.emit("bookingFailed","Movie date not found while cancelling booking.");
+
+    const movie = movies.find((m)=>m.id === booking.movieId);
+    if (!movie) {
+        bookingEmitter.emit("bookingFailed","Movie data not found while cancelling booking.");
         return null;
     }
-    const showtime = movie.showtimes.find((show)=>show.time.toLowerCase() === booking.time.toLowerCase());
-    if(!showtime){
+
+    const showtime = movie.showtimes.find((show)=>show.time.toLowerCase()===booking.time.toLowerCase());
+    if (!showtime) {
         bookingEmitter.emit("bookingFailed","Showtime data not found");
         return null;
     }
 
     //restore seats
-    showtime.seatAvailable += booking.seatCount;
+    showtime.seatsAvailable +=booking.seatCount;
 
-    //clear current Booking
+    // clear current Booking
     clearCurrentBooking();
 
     bookingEmitter.emit("bookingCancelled",booking);
 
     return booking;
 }
+
 module.exports = {
     cancelBooking
 };
