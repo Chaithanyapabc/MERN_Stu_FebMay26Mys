675a59a07b9a051a9b9c26c94e59be11a60fb7d9
Chaithanyapabc <pchaithanya684@gmail.com>
2026-04-08 16:53:48 +0530

    Does the ticket booking

---
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/controllers/bookingController.js
@@ -0,0 +1,92 @@
+//Does the ticket booking
+
+const movies = require("../data/movies");
+
+const CustomError = require("../utils/customError");
+
+const bookings = [];
+
+function bookingValidationHandler(req,res,next){
+    const {movieId,showtimeId,seatCount} = req.body;
+
+    if(!movieId || !showtimeId || !seatCount){
+        return next(new CustomError("movieId,showtimeId,seatCount are requirred",404));
+    }
+    next();
+}
+
+function createBooking(req,res,next){
+    try{
+        const {movieId,showtimeId,seatCount} = req.body;
+
+        const movie = movies.find((m)=>m.id === Number(movieId));
+        if(!movie){
+            return next(new CustomError("Movie not found",404));
+        }
+
+        const showtime = movies.showtimes.find((m)=>m.id === Number(showtimeId));
+        if(!showtime){
+            return next(new CustomError("Showtime not found",404));
+        }
+        if(showtime.seatAvailable<Number(seatCount)){
+            return next(new CustomError("Not enough seats available",404))
+        }
+
+        showtime.seatAvailable -= Number(seatCount);
+
+        const booking = {
+            id:bookings.length+1,
+            userId:req.user.id,
+            userName:req.user.name,
+            movieId:movie.id,
+            movieTitle:movie.title,
+            showtimeId:showtime.id,
+            showtime:showtime.time,
+            seatCount:Number(seatCount)
+        };
+        booking.push(booking);
+
+        res.status(201).json({
+            success:true,
+            message:"Booking created successfully.",
+            data:booking
+        });
+    }
+    catch(error){
+        next(error);
+    }
+}
+
+function getAllBookings(req,res,next){
+    try{
+        res.status(200).json({
+            success:true,
+            count:bookings.length,
+            data:bookings
+        });
+    }
+    catch(error){
+        next(error);
+    }
+}
+
+function getMyBookings(req,res,next){
+    try{
+        const userBookings = bookings.filter((booking)=>booking.userId === req.user.id);
+        res.status(200).json({
+            success:true,
+            count:userbookings.length,
+            data:userbookings
+        });
+    }
+    catch(error){
+        next();
+    }
+}
+
+module.exports = {
+    bookingValidationHandler,
+    createBooking,
+    getAllBookings,
+    getMyBookings
+}
