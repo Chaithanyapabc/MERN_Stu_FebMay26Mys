431e1af3d97b0a6e05d80dc08a0cde8308a13984
Chaithanyapabc <pchaithanya684@gmail.com>
2026-04-15 12:47:38 +0530

    Date Fundamentals

---
+++ W8/D4/P1.js
@@ -0,0 +1,30 @@
+//Date Fundamentals
+const mongoose = require("mongoose");
+async function main(){
+    try{
+        await mongoose.connect('mongodb://127.0.0.1:27017/datedb');
+        console.log("Connected to MongoDB");
+        const schema = new mongoose.Schema({
+            name:String,
+            createdAt:{
+                type:Date,
+                default:Date.now
+            }
+        });
+        const Model = mongoose.model("DateFund",schema);
+        await Model.deleteMany();
+
+        const doc = await Model.create({
+            name:"test"
+        });
+        console.log("Document:",doc);
+    }
+    catch(error){
+        console.log("Error",error.message);
+    }
+    finally{
+        await mongoose.disconnect();
+        console.log("Disconnected from DB.");
+    }
+}
+main();
