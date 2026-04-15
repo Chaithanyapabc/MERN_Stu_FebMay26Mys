399392f68f2be3e3e04939159f107e7df6aebdd8
Chaithanyapabc <pchaithanya684@gmail.com>
2026-04-15 14:05:07 +0530

    Custom EventEmitter for system events

--- LinkedIn_Clone/events.js
+++ LinkedIn_Clone/events.js
@@ -1,27 +1,8 @@
+//Custom EventEmitter for system events
 const EventEmitter = require("events");
-const chalk = require("chalk");
 
-const eventEmitter = new EventEmitter();
+class AppEmitter extends EventEmitter {}
 
-// Event listeners
-eventEmitter.on("profileCreated", (user) => {
-    console.log(chalk.green(`Profile created for ${user.name}`));
-});
+const emitter = new AppEmitter();
 
-eventEmitter.on("connectionRequested", () => {
-    console.log(chalk.blue("Connection request sent"));
-});
-
-eventEmitter.on("connectionAccepted", () => {
-    console.log(chalk.green("Connection accepted"));
-});
-
-eventEmitter.on("postCreated", () => {
-    console.log(chalk.cyan("Post created successfully"));
-});
-
-eventEmitter.on("operationFailed", (msg) => {
-    console.log(chalk.red(`${msg}`));
-});
-
-module.exports = eventEmitter;
+module.exports = emitter;
