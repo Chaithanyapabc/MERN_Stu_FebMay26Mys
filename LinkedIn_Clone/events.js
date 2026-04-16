aa302d3a0f3eac31c2f1842e00cf9f52a83c8a36
Chaithanyapabc <pchaithanya684@gmail.com>
2026-04-16 09:17:08 +0530

    Removerd file

--- LinkedIn_Clone/events.js
+++
@@ -1,8 +0,0 @@
-//Custom EventEmitter for system events
-const EventEmitter = require("events");
-
-class AppEmitter extends EventEmitter {}
-
-const emitter = new AppEmitter();
-
-module.exports = emitter;
