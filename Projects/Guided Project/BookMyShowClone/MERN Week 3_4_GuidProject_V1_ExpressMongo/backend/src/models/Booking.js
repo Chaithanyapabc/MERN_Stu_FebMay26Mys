2e033d4c520b1206cb065912f828eab05e3e04b6
Chaithanyapabc <pchaithanya684@gmail.com>
2026-04-21 12:49:43 +0530

    Created booking schema ,model, added validation,created index and exported models

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/models/Booking.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/models/Booking.js
@@ -37,15 +37,15 @@ const bookingSchema=new mongoose.Schema({
 //Add validation
 bookingSchema.pre("save",function(next){
     if(this.seats.length ===0 ){
-        return next(new Error ("At least one seat must be selected"))
+        return next(new Error ("At least one seat must be selected"));
     }
     if(this.totalSeats!=this.seats.length){
-        return next(new Error("Seat count mismatch"))
+        return next(new Error("Seat count mismatch"));
     }
     next()
 })
 
 //compound index
-bookingSchema.index({userId:1,showId:1})
+bookingSchema.index({userId:1,showId:1});
 
-module.exports=mongoose.model("Booking",bookingSchema)
+module.exports=mongoose.model("Booking",bookingSchema);
