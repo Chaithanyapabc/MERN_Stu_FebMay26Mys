822d1329af231daba8b76d268725e55355965ab5
Chaithanyapabc <pchaithanya684@gmail.com>
2026-04-22 09:50:39 +0530

    Import corrected

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/app.js
@@ -1,15 +1,18 @@
-//Basic app setup
-
 const express = require("express");
+const authRoutes = require("./src/routes/auth.routes");
 
 const app = express();
 
 app.use(express.json());
+app.use("/api/auth",authRoutes);
 
+//Base URL
 app.get("/",(req,res)=>{
     res.status(200).json({
-         success:true,
-        message:"Movie booking API is running...",
+        success:true,
+        message: "Movie booking API is running...",
     });
 });
+
+
 module.exports = app;
