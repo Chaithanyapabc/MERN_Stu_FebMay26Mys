ceba2a5dfbe13c2233e889905ec07394c6a75526
Chaithanyapabc <pchaithanya684@gmail.com>
2026-04-28 12:36:06 +0530

    React application to learn Components concept

---
+++ W9/D2/my-app/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+import './App.css'
+import { FunctionName } from './components/FunctionalCompOne.jsx'
+import { FunctionalComponentsBasics } from './components/FunctionalComponentsBasics.jsx'
+import { ClassComponentsBasics } from './components/ClassComponentsBasics.jsx'
+import { FunctionalComp } from './components/FunctionalComponentAdv.jsx'
+
+function App() {
+
+  return (
+    //Fragment in react : <> </> 
+    <>
+      <FunctionName /> {/*Component Name */}
+      <FunctionalComponentsBasics />
+      <ClassComponentsBasics />
+      <FunctionalComp />
+    </>
+  )
+}
+
+export default App
