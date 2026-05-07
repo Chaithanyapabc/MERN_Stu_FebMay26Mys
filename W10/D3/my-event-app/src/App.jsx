31633c8ee141187b3108674b0b7f285b3c02a6e0
Chaithanyapabc <pchaithanya684@gmail.com>
2026-05-07 10:33:23 +0530

    React Event Object

--- W10/D3/my-event-app/src/App.jsx
+++ W10/D3/my-event-app/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import { EventBasic } from "./components/P1"
+import { EventObject } from "./components/P2"
 
 function App() {
 
   return (
     <>
-      <EventBasic />
+      {/* <EventBasic /> */}
+      <EventObject />
     </>
   )
 }
