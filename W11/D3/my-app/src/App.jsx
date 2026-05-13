99febccc8ee651a5e6b8d4282f41315e1098eaed
Chaithanyapabc <pchaithanya684@gmail.com>
2026-05-13 16:25:17 +0530

    Axios usage with useEffect

--- W11/D3/my-app/src/App.jsx
+++ W11/D3/my-app/src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 import { ManagingApiState } from "./components/P1"
 import { UseEffectLifecycle } from "./components/P2"
+import { AxiosLifecycle } from "./components/P3"
 
 function App() {
 
   return (
     <>
       {/* <ManagingApiState /> */}
-      <UseEffectLifecycle />
+      {/* <UseEffectLifecycle /> */}
+      <AxiosLifecycle/>
     </>
   )
 }
